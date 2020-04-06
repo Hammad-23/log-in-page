31dbcae3c41dd7987db1290d61a636c49f513f1a
Hammad-23 <62185607+Hammad-23@users.noreply.github.com>
2020-04-06 23:05:59 +0500

    sign up & sign in

--- recieve.js
+++ recieve.js
@@ -1,22 +1,53 @@
 function add(){
-   var data= document.getElementById("email").value
-   var pas= document.getElementById("pass").value
-    console.log(data)
-    console.log(pas)
-    var specific= "hsyed2633@gmail.com"
-var specific2= "32271147"
-if(data===specific&&pas===specific2){
+
+   var email= document.getElementById("email").value
+   var password= document.getElementById("pass").value
     
-    alert("yes you are correct")
-} else {
-    alert("your data is incorrect")
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(res=>{
+        window.location.href = "signIn.html"
+         
+Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your work has been Done',
+    showConfirmButton: false,
+    timer: 1500
+  })
+    })
+
 }
-}
-function cursor(){
-    document.getElementById("img").style.height="300px"
-    document.getElementById("img").style.width="300px"
-}
-function mouse(){
-    document.getElementById("img").style.height="150px"
-    document.getElementById("img").style.width="150px"
+// function cursor(){
+//     document.getElementById("img").style.height="300px"
+//     document.getElementById("img").style.width="300px"
+// }
+// function mouse(){
+//     document.getElementById("img").style.height="150px"
+//     document.getElementById("img").style.width="150px"
+// }
+
+function signIn(){
+    
+      
+    var email= document.getElementById("email").value
+   var password= document.getElementById("password").value
+
+   firebase.auth().signInWithEmailAndPassword(email, password).then(res=>{
+        
+Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your work has been Done',
+    showConfirmButton: false,
+    timer: 1500
+  })
+   }).catch(error=>{
+        
+Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: error.message,
+    footer: '<a href>Why do I have this issue?</a>'
+  })
+   })
+    
 }
